4cb2f15c44c2bb00d7f9a668507b5b175c404be8
Techy-2002ashik <2002ashik19@gmail.com>
2026-05-19 18:17:42 +0600

    Labtask2

--- main.cpp
+++ main.cpp
@@ -1,67 +1,141 @@
-#include <iostream>
-#include <cctype>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+
 using namespace std;
 
-bool isNumericConstant(string s) {
-    if (s.empty()) return false;
+vector<string> keywords ={
+"int", "float", "double", "char", "string",
+"if", "else", "for", "while", "return", "void", "class", "public", "private", "protected", "include", "using",
+"namespace", "main", "switch", "case", "break", "continue"
+};
 
-    for (char c : s) {
-        if (!isdigit(c))
-            return false;
+bool myIsDigit( char c){
+return c >= '0' && c <= '9';
+}
+
+bool myIsAlpha(char c){
+return (c >= 'A' && c <= 'Z')||(c >= 'a' && c <= 'z' );
+}
+
+bool myIsAlnum(char c){
+return myIsAlpha(c) || myIsDigit(c);
+}
+
+bool myIsKeyword(const string & token){
+for (const string & kw : keywords){
+    if(kw == token) return true;
+}
+return false;
+}
+
+bool myIsNumber(const string & token){
+if(token.empty()) return false;
+
+for (char c : token){
+    if(!myIsDigit(c)) return false;
+}
+return true;
+}
+
+bool myIsIdentifier(const string & token){
+if(token.empty()) return false;
+
+if(!(myIsAlpha(token[0]) || token[0] == '_')) return false;
+
+for(char c: token){
+    if(!(myIsAlnum(c) || c == '_')) return false;
+}
+return true;
+}
+
+bool myIsSymbol(const string & token){
+string symbols = ";(){}[]=+-*/<>,.";
+if(token.size()!=1) return false;
+
+for( char s: symbols){
+    if(token[0] == s)return true;
+}
+return false;
+}
+
+vector<string> tokenize(const string & line){
+vector<string> tokens;
+string current = "";
+
+ for (size_t i = 0; i < line.size(); i++) {
+        char c = line[i];
+
+        // If symbol → flush current and add symbol
+        string symbols = ";{}()[]=+-*/<>,.";
+        bool isSym = false;
+        for (char s : symbols) {
+            if (c == s) { isSym = true; break; }
+        }
+
+        if (isSym) {
+            if (!current.empty()) {
+                tokens.push_back(current);
+                current.clear();
+            }
+            tokens.push_back(string(1, c));
+        }
+        else if (c == ' ' || c == '\t') {
+            if (!current.empty()) {
+                tokens.push_back(current);
+                current.clear();
+            }
+        }
+        else {
+            current += c;
+        }
     }
-    return true;
+
+    if (!current.empty()) tokens.push_back(current);
+    return tokens;
+
 }
-bool isOperator(char c) {
-    return (c == '+' || c == '-' || c == '*' ||
-            c == '/' || c == '%' || c == '=');
+int main(){
+ifstream file("input.txt");
+
+if(!file){
+    cout<<" Error: Please enter the valid name of file on source code!"<< endl;
+    return 1;
 }
-bool isComment(string s) {
-    if (s.size() >= 2) {
-        if (s.substr(0, 2) == "//")
-            return true;
-        if (s.substr(0, 2) == "/" && s.substr(s.size() - 2) == "/")
-            return true;
+
+string line;
+
+cout<<"++++File Content++++\n";
+while (getline(file, line)){
+cout<< line<< endl;
+}
+file.clear();
+file.seekg(0, ios ::beg);
+
+cout<<"\n++Tokens & it's classifications++\n";
+
+while (getline(file, line)){
+    vector<string> tokens = tokenize(line);
+    for(const string & token : tokens){
+        if(myIsKeyword(token)){
+            cout<< token <<"--->>Keyword\n";
+        }
+        else if(myIsNumber(token)){
+            cout<< token <<"--->>Number\n";
+        }
+        else if(myIsIdentifier(token)){
+            cout<< token <<"--->>Identifier\n";
+        }
+        else if(myIsSymbol(token)){
+            cout<< token <<"--->>Symbols\n";
+        }
+        else{
+            cout<< token <<"--->>Unknown\n";
+        }
     }
-    return false;
 }
-bool isIdentifier(string s) {
-    if (s.empty())
 
-    return false;
-
-    if (!(isalpha(s[0]) || s[0] == '_'))
-        return false;
-
-    for (int i = 1; i < s.length(); i++) {
-        if (!(isalnum(s[i]) || s[i] == '_'))
-            return false;
-    }
-    return true;
-}
-int main() {
-    string input;
-    char op;
-
-
-    cout << "Enter a value to check numeric constant: ";
-    cin >> input;
-    cout << (isNumericConstant(input) ? "Numeric Constant\n" : "Not Numeric\n");
-
-
-    cout << "Enter a character to check operator: ";
-    cin >> op;
-    cout << (isOperator(op) ? "Operator\n" : "Not Operator\n");
-
-
-    cout << "Enter a comment line: ";
-    cin.ignore();
-    getline(cin, input);
-    cout << (isComment(input) ? "Comment Line\n" : "Not a Comment\n");
-
-
-    cout << "Enter an identifier: ";
-    cin >> input;
-    cout << (isIdentifier(input) ? "Valid Identifier\n" : "Invalid Identifier\n");
-
-    return 0;
+file.close();
+return 0;
 }
